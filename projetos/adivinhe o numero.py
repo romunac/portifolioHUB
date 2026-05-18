2c21f0c07136a11105e1095a3339c03cde28d41f
romunac <lucasvinhal3001@gmail.com>
2026-05-17 23:10:50 -0300

    codigo adivinha

--- projetos/adivinhe o numero.py
+++ projetos/adivinhe o numero.py
@@ -1,1 +1,22 @@
+import random
+import time
 
+print("=== Adivinhe o Número ===")
+print("Estou pensando em um número de 1 a 100...")
+time.sleep(1)
+
+numero_secreto = random.randint(1, 100)
+tentativas = 0
+
+while True:
+    palpite = int(input("Digite seu palpite: "))
+    tentativas += 1
+
+    if palpite < numero_secreto:
+        print("Muito baixo!")
+    elif palpite > numero_secreto:
+        print("Muito alto!")
+    else:
+        print(f"\nParabéns! Você acertou o número {numero_secreto}!")
+        print(f"Você precisou de {tentativas} tentativas.")
+        break
