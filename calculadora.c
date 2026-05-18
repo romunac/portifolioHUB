81d1ca45e3df7d50a75dae5384b8d53133c97e08
romunac <lucasvinhal3001@gmail.com>
2026-05-17 22:21:44 -0300

    calculadora nota

--- calculadora.c
+++ calculadora.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() {
+    float num1, num2;
+    char operador;
+//a calculara ainda e bem simples, ela tera futuras melhorias.
+    printf("Digite uma expressao: ");
+    scanf("%f %c %f", &num1, &operador, &num2);
+
+    switch(operador) {
+        case '+':
+            printf("Resultado: %.2f\n", num1 + num2);
+            break;
+        case '-':
+            printf("Resultado: %.2f\n", num1 - num2);
+            break;
+        case '*':
+            printf("Resultado: %.2f\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("Resultado: %.2f\n", num1 / num2);
+            else
+                printf("Erro: divisao por zero!\n");
+            break;
+        default:
+            printf("Operador invalido!\n");
+    }
+
+    return 0;
+}
